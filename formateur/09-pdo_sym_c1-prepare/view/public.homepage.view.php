532c75d30d8fa55d305e18f3864592949fd20cd1
souli <chair.soulaiman.map@gmail.com>
2025-03-28 14:08:39 +0100

    add

--- formateur/09-pdo_sym_c1-prepare/view/public.homepage.view.php
+++ formateur/09-pdo_sym_c1-prepare/view/public.homepage.view.php
@@ -12,7 +12,7 @@
     <?php
     foreach($menu as $item):
     ?>
-    <a href="?section=<?=$item['section_slug']?>"><?=$item['section_title']?></a> |
+    <a href="?section=<?=$item['section_slug']?>">    <?=$item['section_title']?> </a> |
     <?php
     endforeach;
     ?>
